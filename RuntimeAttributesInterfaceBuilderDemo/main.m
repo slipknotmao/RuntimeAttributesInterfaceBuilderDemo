f15e102eea0eae601bf0b0298419b9bb61ef5bb3
slipknotmao <slipknotmao@126.com>
2015-07-02 14:32:48 +0800

    Initial Commit

---
+++ RuntimeAttributesInterfaceBuilderDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RuntimeAttributesInterfaceBuilderDemo
+//
+//  Created by 华晓春 on 15/7/2.
+//  Copyright (c) 2015年 华晓春. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
